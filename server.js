bd22229904c9ac9f5ab232a6cd6b699b06b33606
Biosite Medical Instrument <biosite.ga@gmail.com>
2025-08-06 06:46:04 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-// Directly serve the HTML inside node_modules
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'node_modules', 'index.html'));
+app.use(express.static(path.join(__dirname, 'dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-// Optionally serve static files too
-app.use('/assets', express.static(path.join(__dirname, 'node_modules')));
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
