8c38179d7d51ea9ec8118cdf13066738ab53c3d5
Biosite Medical Instrument <biosite.ga@gmail.com>
2025-08-06 06:53:36 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import React from 'react';
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist'  // 👈 THIS LINE IS IMPORTANT FOR DEPLOYMENT
+  }
 });
